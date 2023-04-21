3a638e5cd551e5c9611d36bf2b14cc9e44d3485d
Elham Raeisi <zaribaf.elham@gmail.com>
2023-04-21 10:49:15 +0200

    fix unit tests

--- src/tests/Employee.spec.js
+++ src/tests/Employee.spec.js
@@ -4,30 +4,6 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import Employee from './../pages/Employee';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-const mockEmployees = [
-  {
-    id: 1,
-    name: 'John',
-    lastname: 'Doe',
-    birthday: '1990-01-01',
-    startdate: '2020-01-01',
-    street: '123 Main St',
-    state: 'CA',
-    zipcode: '12345',
-    department: { label: 'Sales' }
-  },
-  {
-    id: 2,
-    name: 'Jane',
-    lastname: 'Smith',
-    birthday: '1995-02-02',
-    startdate: '2021-02-02',
-    street: '456 Oak Ave',
-    state: 'NY',
-    zipcode: '67890',
-    department: { label: 'Marketing' }
-  }
-]
 jest.mock('react-redux', () => ({
   useDispatch: jest.fn(),
   useSelector: jest.fn(),
@@ -47,21 +23,6 @@ describe('Table', () => {
     const header = screen.getByText('Current Employees');
     expect(header).toBeDefined();
   });
-
-  // it('displays employee data correctly', () => {
-  //   jest.mock('react-redux', () => ({
-  //     useDispatch: jest.fn(),
-  //     useSelector: jest.fn(),
-  //   }));
-  //   render(<Employee />, { wrapper: Router });
-
-  //   const johnName = screen.getByText('John');
-  //   const janeStartDate = screen.getByText('2/2/2021');
-  //   const marketingDepartment = screen.getByText('Marketing');
-  //   expect(johnName).toBeDefined();
-  //   expect(janeStartDate).toBeDefined();
-  //   expect(marketingDepartment).toBeDefined();
-  // });
 
 });
 
